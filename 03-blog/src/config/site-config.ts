5049cc048baae957807cd93d7bac9954670cff34
CJavatX <cdpm98@hotmail.com>
2024-07-31 17:58:22 -0600

    SECCIÓN 5 / CAP 10 - EN CURSO

---
+++ 03-blog/src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: "CJavatX Blog",
+  description: "Blog oficial de FSociety, creado por CJavatX",
+};
---
+++ 03-blog/src/content/config.ts
@@ -0,0 +1,25 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: z.string(),
+
+    // Relación
+    author: z.string(),
+
+    // Relación
+    tags: z.array(z.string()),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  blog: blogCollection,
+};
