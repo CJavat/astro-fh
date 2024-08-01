350892578e0464c4a1335d9a2f82c927d08fc127
CJavatX <cdpm98@hotmail.com>
2024-08-01 13:16:41 -0600

    SECCIÓN 6 - TERMINADO

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
@@ -14,10 +14,28 @@ const blogCollection = defineCollection({
       }),
 
       // Relación
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // Relación
       tags: z.array(z.string()),
+
+      // Boolean
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
     }),
 });
 
@@ -25,4 +43,5 @@ const blogCollection = defineCollection({
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
