f8a789d92ea8436a4b569a728b9a2444a5d9760f
CJavat <cdpm98@hotmail.com>
2024-10-13 22:13:03 -0600

    Server Islands

---
+++ 06-reviews-app/astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  experimental: {
+    serverIslands: true,
+  },
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
